dbdcc618752ab940a075fe0bfb0289abd226732b
FrankvVeelen <frankvanveelen@hotmail.com>
2019-06-28 14:17:42 +0200

    idk wat ik heb veranderd

--- fitness.py
+++ fitness.py
@@ -9,11 +9,11 @@ class Fitness:
         self.num_objectives = num_objectives
         self.weight_vector = weight_vector
 
-        self.fitnesses = [None]*num_objectives # fitnesses is array of fitness[objective][genotype]
-        for i in range(self.num_objectives):
-            self.fitnesses[i] = [None]*self.size_population
+        self.fitnesses = [None]*size_population # fitnesses is array of fitness[objective][genotype]
+        for i in range(self.size_population):
+            self.fitnesses[i] = [None]*self.num_objectives
 
-        self.best_genotype_1d = [inf] * num_objectives  # called z[i] in paper
+        self.z_optimum = [inf] * num_objectives  # called z[i] in paper
 
     def calculate_fitness_pop(self, population, problem):
         if self.fitness_type == "MO_tsp":
@@ -36,8 +36,7 @@ class Fitness:
                         fitness += distance_between_nodes(problem.nodes[previousCity], problem.nodes[city],
                                                           objective)
                     previousCity = city
-                self.fitnesses[objective][genotype] = fitness
-            print(self.fitnesses[0])
+                self.fitnesses[genotype][objective] = fitness
 
     def MO_tsp_fitness_genotype(self, genotype, problem): # TODO use this in the pop function
         fitness_genotype = [0]*self.num_objectives
@@ -56,16 +55,16 @@ class Fitness:
         # find the best genotype for every objective separately
         for objective in range(self.num_objectives):
             for genotype in range(self.size_population):
-                if self.fitnesses[objective][genotype] < self.best_genotype_1d[objective]:
-                    self.best_genotype_1d[objective] = self.fitnesses[objective][genotype]
+                if self.fitnesses[genotype][objective] < self.z_optimum[objective]:
+                    self.z_optimum[objective] = self.fitnesses[genotype][objective]
 
     def calculate_z_optimums_genotype(self, genotype_fitness):
         # Check if genotype is better than optimum for each obj separately thus far
         for objective in range(self.num_objectives):
             print("fitness new child: " + str(genotype_fitness[objective]),
-                  "old best: " + str(self.best_genotype_1d))
-            if genotype_fitness[objective] < self.best_genotype_1d[objective]:
-                self.best_genotype_1d[objective] = genotype_fitness[objective]
+                  "old best: " + str(self.z_optimum))
+            if genotype_fitness[objective] < self.z_optimum[objective]:
+                self.z_optimum[objective] = genotype_fitness[objective]
                 print("Z optimum was updated")
 
 def distance_between_nodes(nodeA, nodeB, objective):
