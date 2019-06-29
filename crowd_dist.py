ba6077153215f270eb68773616418751b99e41b8
Wilfredal <38689019+Wilfredal@users.noreply.github.com>
2019-06-29 15:34:13 +0200

    Update crowd_dist.py

--- crowd_dist.py
+++ crowd_dist.py
@@ -1,12 +1,14 @@
 import math
 
 def crowding_distance_assignment(I):
-	l = len(I)
-	I_dist = [0] * l
+    l = len(I)
+    I_dist = [0] * l
 
-	for m in range(len(I.fitness))
-		I.sort(key=lambda population: population.fitness[m])
-		I_dist[0] = I_dist[l-1] = math.inf
-		
-		for i in range(1, l-2)
-			I_dist[i] = I_dist[i] + (I[i+1].fitness[m] - I[i-1].fitness[m])/(I[i].fitness[-1] - I[i].fitness[0])
+    for m in range(len(I[0].fitness)):
+        I.sort(key=lambda population: population.fitness[m])
+        I_dist[0] = I_dist[-1] = math.inf
+
+        for i in range(1, l - 1):
+            I_dist[i] = I_dist[i] + (I[i + 1].fitness[m] - I[i - 1].fitness[m]) / (I[-1].fitness[m] - I[0].fitness[m])
+
+	return I_dist
